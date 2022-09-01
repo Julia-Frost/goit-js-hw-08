ab0c9f8be1d34ae798ef041507ff5288f59a8577
Julia-Frost <juliafrostwork@gmail.com>
2022-09-01 20:49:20 +0300

    add files

---
+++ src/js/01-gallery.js
@@ -0,0 +1,56 @@
+// Add imports above this line
+import { galleryItems } from "./gallery-items";
+// Change code below this line
+
+// Створення розмітки
+
+const galleryEl = document.querySelector(".gallery");
+const list = createImgList(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", list);
+
+function createImgList(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="large-image.jpg">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>`;
+    })
+    .join("");
+}
+
+// Додання слухача подій на <div class="gallery">
+
+galleryEl.addEventListener("click", onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  // Підключення Лайтбокс
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  // Закриття по кліку на Escape
+
+  window.addEventListener("keydown", onEscapeDown);
+  function onEscapeDown(event) {
+    if (event.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  }
+}
