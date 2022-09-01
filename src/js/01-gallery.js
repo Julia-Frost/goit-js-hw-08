fd89cddda795d588d24e39486b36b0616df99012
Julia-Frost <juliafrostwork@gmail.com>
2022-09-01 21:47:08 +0300

    add code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,19 @@
 // Add imports above this line
-import { galleryItems } from "./gallery-items";
+import { galleryItems } from './gallery-items';
 // Change code below this line
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Створення розмітки
 
-const galleryEl = document.querySelector(".gallery");
+const galleryEl = document.querySelector('.gallery');
 const list = createImgList(galleryItems);
 
-galleryEl.insertAdjacentHTML("beforeend", list);
+galleryEl.insertAdjacentHTML('beforeend', list);
 
 function createImgList(galleryItems) {
   return galleryItems
@@ -24,17 +30,17 @@ function createImgList(galleryItems) {
                 </a>
             </div>`;
     })
-    .join("");
+    .join('');
 }
 
 // Додання слухача подій на <div class="gallery">
 
-galleryEl.addEventListener("click", onImgClick);
+galleryEl.addEventListener('click', onImgClick);
 
 function onImgClick(event) {
   event.preventDefault();
 
-  if (!event.target.classList.contains("gallery__image")) {
+  if (!event.target.classList.contains('gallery__image')) {
     return;
   }
   // Підключення Лайтбокс
@@ -46,9 +52,9 @@ function onImgClick(event) {
 
   // Закриття по кліку на Escape
 
-  window.addEventListener("keydown", onEscapeDown);
+  window.addEventListener('keydown', onEscapeDown);
   function onEscapeDown(event) {
-    if (event.code !== "Escape") {
+    if (event.code !== 'Escape') {
       return;
     }
     instance.close();
