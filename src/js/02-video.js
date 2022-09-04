c69ad7292bfe276bdc77694a3a950f84df4cea07
Julia-Frost <juliafrostwork@gmail.com>
2022-09-04 23:03:53 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+// import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(videoTimeStop, 1000));
+
+function videoTimeStop(timeupdate) {
+  let timePause = timeupdate.seconds;
+  console.log(timePause);
+
+  localStorage.setItem('videoplayer-current-time', timePause);
+}
+
+function afterUpdatePage() {
+  let savedTimePlayer = localStorage.getItem('videoplayer-current-time');
+  if (savedTimePlayer) {
+    player
+      .setCurrentTime(savedTimePlayer)
+      .then(function () {})
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+}
+
+afterUpdatePage();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const email = document.querySelector('.feedback-form input');
+
+const STG_KEY = 'formStorage';
+const formStorage = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onNameInput, 500));
+textarea.addEventListener('input', throttle(onTextAreaInput, 500));
+email.addEventListener('input', throttle(onTextAreaInput, 500));
+
+function onNameInput(event) {
+  formStorage[event.target.name] = event.target.value;
+
+  console.log(formStorage);
+
+  localStorage.setItem(STG_KEY, JSON.stringify(formStorage));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(formStorage);
+  event.target.reset();
+  localStorage.removeItem(STG_KEY);
+}
+
+function onTextAreaInput(event) {
+  const messageValue = event.target.value;
+  localStorage.setItem(STG_KEY, messageValue);
+}
+
+function onRefreshPage() {
+  const localSaveMessage = JSON.parse(localStorage.getItem(STG_KEY));
+
+  if (localSaveMessage?.email) {
+    email.value = localSaveMessage.email;
+  }
+  if (localSaveMessage?.message) {
+    textarea.value = localSaveMessage.message;
+  }
+}
+
+onRefreshPage();
