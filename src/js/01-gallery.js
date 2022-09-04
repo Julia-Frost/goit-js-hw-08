9ce46925361c9fe4ecd6e981c86645e5e9165782
Julia-Frost <juliafrostwork@gmail.com>
2022-09-04 22:45:10 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,9 +2,13 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 const galleryAlbum = document.querySelector('.gallery');
+
 galleryAlbum.addEventListener('click', openGalleryItem);
-// Добавляет картинки из объекта в разметку
+
+// Додає зображення з масиву
+
 function addItemToGallery({ preview, original, description }) {
   return `<a class="gallery__item" href="${original}">
             <img class="gallery__image"
@@ -12,16 +16,20 @@ function addItemToGallery({ preview, original, description }) {
             alt="${description}" />
         </a>`;
 }
-// Делаем один бойльшой файл разметки
-let addNewImage = galleryItems.map(addItemToGallery).join('');
-// Вставляет в разметку разметку картинки
+
+// Створює розмітку
+
+const addNewImage = galleryItems.map(addItemToGallery).join('');
+
 galleryAlbum.insertAdjacentHTML('afterbegin', addNewImage);
-// Открывает модальное окно с картинкой по клику (большой, оригинальной)
+
+// Відкриває зображення по кліку
+
 function openGalleryItem(event) {
   event.preventDefault();
-  // Достукиваемся до дата-атрибута по клику
   const imageSrc = event.target.dataset.source;
 }
-let gallery = new SimpleLightbox('.gallery a');
-let captionsData = (gallery.options.captionsData = 'alt');
-let captionDelay = (gallery.options.captionDelay = 250);
+
+const gallery = new SimpleLightbox('.gallery a');
+const captionsData = (gallery.options.captionsData = 'alt');
+const captionDelay = (gallery.options.captionDelay = 250);
